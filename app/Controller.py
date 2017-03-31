d70b33ffbc89ea2500eb9d688f549cc481021402
AnuragNaik <anurag26n.nitc@gmail.com>
2017-03-31 15:34:31 +0530

    added some functions to debug

--- app/Controller.py
+++ app/Controller.py
@@ -1,18 +1,57 @@
 import psycopg2
 from model.SchemaGraph import SchemaGraph
+from model.NodeMapper import NodeMapper
+from model.ParseTree import ParseTree
+from model.NLParser import NLParser
+
 
 class Controller:
     conn = None
+    nodeMapper = None
+    parser = None
+    schema = None
+    processing = False
+    parseTree = None
+    mappingNodes = False
+
     def __init__(self):
+        self.startConnection()
+        self.nodeMapper = NodeMapper()
+        self.parser = NLParser()
+        print "Controller initialized."
         pass
 
     def startConnection(self):
         """ Connect to MySQL database """
         try:
-            conn = psycopg2.connect("dbname='dblp' user='postgres' host='localhost' password='Codechef'")
+            self.conn = psycopg2.connect("dbname='dblp' user='postgres' host='localhost' password='Codechef'")
         except:
             print "I am unable to connect to the database"
-        schema = SchemaGraph(conn)
+        print "connected to database..."
+        self.schema = SchemaGraph(self.conn)
+
+    def startMappingNodes(self):
+        if self.mappingNodes:
+            return
+        self.mappingNodes = True
+        #
+        # iter = parseTree.iterator();
+        # if (!iter.hasNext()) {
+        #     finishNodesMapping();
+        #     return;
+        # }
+        # node = iter.next();
+        # List < NodeInfo > choices = nodeMapper.getNodeInfoChoices(node, schema);
+        # if (choices.size() == 1) {chooseNode(choices.get(0));}
+        # else {setChoicesOnView(choices);}
+
+    def processNaturalLanguage(self, input):
+        if self.processing:
+            print "\nCurrently processing a sentence!\n"
+        else:
+            self.processing = True
+            self.parseTree = ParseTree(input, parser=self.parser)
+            self.startMappingNodes()
 
 
     def closeConnection(self):
@@ -21,6 +60,3 @@ class Controller:
         except Error as e:
             print(e)
         print("Database connection closed")
-
-controller = Controller()
-controller.startConnection()
